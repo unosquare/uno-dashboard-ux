9f6eec7d34ddc6a3ff93a7bd3d9fcda4828082af
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2022-12-06 15:25:43 -0600

    Minor fix

--- sample/index.tsx
+++ sample/index.tsx
@@ -26,45 +26,55 @@ import {
 
 const noOp = () => {};
 
-const Application = () => (
-    <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <NavBar>
-            <NavBarTitle>
-                <h2>Sample App</h2>
-            </NavBarTitle>
-        </NavBar>
-        <AppContainer rows={2} columns={3}>
-            <Card column={1} row={1} direction={Directions.ROW} justify={FlexValues.START}>
-                <Circle value={100} color={Colors.BLUE} size={SizeValues.LARGE} />
-                <Title>
-                    <h2>TOTAL</h2>
-                </Title>
-            </Card>
-            <Card column={2} row={1} direction={Directions.ROW} extraBottom>
-                <Badge value={500} content='Goal' right={30} />
-                <Circle size={SizeValues.EXTRA_SMALL} value={50} color={Colors.GRAY} />
-                <Divider />
-                <CardContent direction={Directions.COLUMN} size={SizeValues.EXTRA_SMALL}>
-                    <h3>Subtitle</h3>
-                    <h4>
-                        <Ellipse color='#4CDD28' small /> This is OK
-                    </h4>
-                </CardContent>
-                <Divider />
-                <Circle size={SizeValues.SMALL} value={100} color={Colors.GRAY} />
-            </Card>
-            <Card column={1} row={2} direction={Directions.COLUMN} fit>
-                <SearchBox search={console.log} />
-                <Loading />
-            </Card>
-            <Card column={2} row={2} direction={Directions.COLUMN} fit>
-                <ToolBar dataTitle='This is a header' onSearch={noOp} onCsvClick={noOp} switchTbl={noOp} />
-            </Card>
-        </AppContainer>
-        <Footer />
-    </ThemeProvider>
-);
+const Application = () => {
+    const [value, setValue] = React.useState(false);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <NavBar>
+                <NavBarTitle>
+                    <h2>Sample App</h2>
+                </NavBarTitle>
+            </NavBar>
+            <AppContainer rows={2} columns={3}>
+                <Card column={1} row={1} direction={Directions.ROW} justify={FlexValues.START}>
+                    <Circle value={100} color={Colors.BLUE} size={SizeValues.LARGE} />
+                    <Title>
+                        <h2>TOTAL</h2>
+                    </Title>
+                </Card>
+                <Card column={2} row={1} direction={Directions.ROW} extraBottom>
+                    <Badge value={500} content='Goal' right={30} />
+                    <Circle size={SizeValues.EXTRA_SMALL} value={50} color={Colors.GRAY} />
+                    <Divider />
+                    <CardContent direction={Directions.COLUMN} size={SizeValues.EXTRA_SMALL}>
+                        <h3>Subtitle</h3>
+                        <h4>
+                            <Ellipse color='#4CDD28' small /> This is OK
+                        </h4>
+                    </CardContent>
+                    <Divider />
+                    <Circle size={SizeValues.SMALL} value={100} color={Colors.GRAY} />
+                </Card>
+                <Card column={1} row={2} direction={Directions.COLUMN} fit>
+                    <SearchBox search={console.log} />
+                    <Loading />
+                </Card>
+                <Card column={2} row={2} direction={Directions.COLUMN} fit>
+                    <ToolBar
+                        dataTitle='This is a header'
+                        onSearch={noOp}
+                        onCsvClick={noOp}
+                        switchTbl={() => setValue(!value)}
+                        isExchange={value}
+                    />
+                </Card>
+            </AppContainer>
+            <Footer />
+        </ThemeProvider>
+    );
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container as any);
--- src/Toolbar/index.tsx
+++ src/Toolbar/index.tsx
@@ -50,7 +50,7 @@ const SearchboxContainer = styled.div`
     justify-content: end;
 `;
 
-export const ToggleOnIcon = (props: any) => (
+const ToggleOnIcon = (props: any) => (
     <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 12 12' {...props}>
         <title>On</title>
         <path
@@ -61,8 +61,8 @@ export const ToggleOnIcon = (props: any) => (
     </svg>
 );
 
-export const ToggleOffIcon = (props: any) => (
-    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' {...props}>
+const ToggleOffIcon = (props: any) => (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 12 12' {...props}>
         <title>Off</title>
         <path
             d='M9 1.5c-.207 0-.401.039-.583.117a1.525 1.525 0 0 0-.478.322 1.525 1.525 0 0 0-.322.478A1.463 1.463 0 0 0 7.5 3c0 .207.039.401.117.583.078.182.186.341.322.478.137.136.296.244.478.322.182.078.376.117.583.117.207 0 .401-.039.583-.117.182-.078.341-.186.478-.322.136-.137.244-.296.322-.478.078-.182.117-.376.117-.583 0-.207-.039-.401-.117-.583a1.525 1.525 0 0 0-.322-.478 1.525 1.525 0 0 0-.478-.322A1.463 1.463 0 0 0 9 1.5ZM3 5.25c-.309 0-.6-.059-.873-.176A2.27 2.27 0 0 1 .926 3.876 2.193 2.193 0 0 1 .75 3c0-.309.059-.6.176-.873A2.27 2.27 0 0 1 2.127.926C2.4.809 2.691.75 3 .75h6c.312 0 .604.059.876.176a2.27 2.27 0 0 1 1.198 1.201c.117.273.176.564.176.873 0 .312-.059.604-.176.876a2.28 2.28 0 0 1-1.198 1.198A2.193 2.193 0 0 1 9 5.25H3ZM0 3c0 .414.078.804.234 1.169.157.365.37.684.642.955.271.272.59.485.955.642C2.196 5.922 2.586 6 3 6h6c.414 0 .804-.078 1.169-.234.365-.157.684-.37.955-.642.272-.271.485-.59.642-.955C11.922 3.804 12 3.414 12 3c0-.414-.078-.804-.234-1.169a3.012 3.012 0 0 0-.642-.955 3.012 3.012 0 0 0-.955-.642A2.944 2.944 0 0 0 9 0H3c-.414 0-.804.078-1.169.234-.365.157-.684.37-.955.642-.272.271-.485.59-.642.955A2.944 2.944 0 0 0 0 3Z'
@@ -87,33 +87,27 @@ const containerStyle = {
     justifyContent: 'end',
 };
 
-export const ToolBar = ({ dataTitle, switchTbl, isExchange, onCsvClick, onSearch, children }: ToolBarSettings) => {
-    const onSwitchTblClick = () => {
-        if (switchTbl) switchTbl();
-    };
-
-    return (
-        <StyledTableTitleContainer>
-            {dataTitle && <StyledTableTitle>{dataTitle}</StyledTableTitle>}
-            {(onSearch || onCsvClick || switchTbl || children) && (
-                <div style={containerStyle}>
-                    {children}
-                    {switchTbl && (
-                        <ActionButtonContainer>
-                            <ActionButton width='max-content' type='button' onClick={onSwitchTblClick}>
-                                {isExchange ? <ToggleOffIcon {...imgStyle} /> : <ToggleOnIcon {...imgStyle} />}
-                                {isExchange ? 'Back to Data' : 'Exchange Rate'}
-                            </ActionButton>
-                        </ActionButtonContainer>
-                    )}
-                    {onCsvClick && <ExportCsvButton onClick={onCsvClick} />}
-                    {onSearch && (
-                        <SearchboxContainer>
-                            <SearchBox search={onSearch} />
-                        </SearchboxContainer>
-                    )}
-                </div>
-            )}
-        </StyledTableTitleContainer>
-    );
-};
+export const ToolBar = ({ dataTitle, switchTbl, isExchange, onCsvClick, onSearch, children }: ToolBarSettings) => (
+    <StyledTableTitleContainer>
+        {dataTitle && <StyledTableTitle>{dataTitle}</StyledTableTitle>}
+        {(onSearch || onCsvClick || switchTbl || children) && (
+            <div style={containerStyle}>
+                {children}
+                {switchTbl && (
+                    <ActionButtonContainer>
+                        <ActionButton width='max-content' type='button' onClick={switchTbl}>
+                            {isExchange ? <ToggleOffIcon {...imgStyle} /> : <ToggleOnIcon {...imgStyle} />}
+                            {isExchange ? 'Back to Data' : 'Exchange Rate'}
+                        </ActionButton>
+                    </ActionButtonContainer>
+                )}
+                {onCsvClick && <ExportCsvButton onClick={onCsvClick} />}
+                {onSearch && (
+                    <SearchboxContainer>
+                        <SearchBox search={onSearch} />
+                    </SearchboxContainer>
+                )}
+            </div>
+        )}
+    </StyledTableTitleContainer>
+);
