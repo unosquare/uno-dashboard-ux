017c82bfb1e298927a641e8a66c074f090dbb61c
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2023-11-27 09:53:01 -0600

    Update Rspack

--- rspack.config.js
+++ rspack.config.js
@@ -1,4 +1,7 @@
+const rspack = require('@rspack/core');
 const path = require('path');
+const ReactRefreshPlugin = require('@rspack/plugin-react-refresh');
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: {
@@ -9,18 +12,69 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
     },
     devServer: { historyApiFallback: true },
-    builtins: {
-        html: [{ template: './public/index.html', publicPath: '/' }],
-        copy: {
-            patterns: ['./public/404.html', './public/manifest.json', './public/favicon.ico'],
-        },
-    },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['postcss-loader'],
             },
+            {
+                test: /\.(j|t)s$/,
+                exclude: [/[\\/]node_modules[\\/]/],
+                loader: 'builtin:swc-loader',
+                options: {
+                    sourceMap: false,
+                    jsc: {
+                        parser: {
+                            syntax: 'typescript',
+                        },
+                        externalHelpers: true,
+                        transform: {
+                            react: {
+                                runtime: 'automatic',
+                                development: !prod,
+                                refresh: !prod,
+                            },
+                        },
+                    },
+                    env: {
+                        targets: 'Chrome >= 110',
+                    },
+                },
+            },
+            {
+                test: /\.(j|t)sx$/,
+                loader: 'builtin:swc-loader',
+                exclude: [/[\\/]node_modules[\\/]/],
+                options: {
+                    sourceMap: false,
+                    jsc: {
+                        parser: {
+                            syntax: 'typescript',
+                            tsx: true,
+                        },
+                        transform: {
+                            react: {
+                                runtime: 'automatic',
+                                development: !prod,
+                                refresh: !prod,
+                            },
+                        },
+                        externalHelpers: true,
+                    },
+                    env: {
+                        targets: 'Chrome >= 110',
+                    },
+                },
+            },
         ],
     },
+    plugins: [
+        new rspack.HtmlRspackPlugin({ template: './public/index.html' }),
+        new rspack.CopyRspackPlugin({
+            patterns: ['./public/404.html', './public/manifest.json', './public/favicon.ico'],
+        }),
+        !prod && new ReactRefreshPlugin(),
+        new rspack.ProgressPlugin({}),
+    ],
 };
