2120718ce4e590ebbee2d616244649c9b061dfaa
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2023-12-26 14:15:30 -0600

    Check

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,28 +19,15 @@ module.exports = {
     extends: 'eslint-config-unosquare',
     plugins: ['eslint-plugin-prettier'],
     rules: {
-        'guard-for-in': 'warn',
-        'react/require-default-props': 'off',
         'react-hooks/exhaustive-deps': 'warn',
         'no-param-reassign': 'warn',
         'spaced-comment': 'warn',
         'import/prefer-default-export': 'off',
-        'react/no-array-index-key': 'warn',
         'react/function-component-definition': [
             'error',
             { unnamedComponents: 'arrow-function', namedComponents: 'arrow-function' },
         ],
         'array-callback-return': ['error', { checkForEach: true }],
-        'sort-imports': [
-            'warn',
-            {
-                ignoreCase: true,
-                ignoreDeclarationSort: true,
-                ignoreMemberSort: false,
-                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-                allowSeparatedGroups: false,
-            },
-        ],
         'react/no-unstable-nested-components': 'warn',
     },
 };
